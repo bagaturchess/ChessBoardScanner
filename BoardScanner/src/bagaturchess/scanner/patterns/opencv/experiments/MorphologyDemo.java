d623fb2348557d2ca23a0fd799032e4e7a39ce5b
Topchiyski <krasimir.topchiyski@sap.com>
2021-03-14 23:16:06 +0200

    Add new experiments

---
+++ BoardScanner/src/bagaturchess/scanner/patterns/opencv/experiments/MorphologyDemo.java
@@ -0,0 +1,11 @@
+package bagaturchess.scanner.patterns.opencv.experiments;
+
+import org.opencv.core.Core;
+
+public class MorphologyDemo {
+    public static void main(String[] args) {
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new Morphology().run(new String[] {"./data/tests/cnn/lichess.org/set1/pictures/test7.png"});
+    }
+}
