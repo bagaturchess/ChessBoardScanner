eee3fdcfb94d2ae5dcb6122f6128f422180d078f
MetatransApps <kitopchiyski@gmail.com>
2021-09-25 22:48:40 +0300

    Minor change

--- BoardScanner/src/bagaturchess/scanner/common/MatchingInfo_BaseImpl.java
+++ BoardScanner/src/bagaturchess/scanner/common/MatchingInfo_BaseImpl.java
@@ -82,7 +82,7 @@ public class MatchingInfo_BaseImpl implements IMatchingInfo {
 		squareIDSet = true;
 		currentSquareID = squareID;
 		latestMessage1 = "Phase [" + currentPhase + "/" + phasesCount + "] " + (int) (100 * currentPhaseProgress) + "%";
-		latestMessage2 = "" + ProviderSwitch.getMLFrameworkDisplayName() + " " +  phaseName.substring(0, phaseName.lastIndexOf('.'));
+		latestMessage2 = "" + ProviderSwitch.getMLFrameworkDisplayName() + ": " +  phaseName.substring(0, phaseName.lastIndexOf('.'));
 		;// + " working on " + ALL_FIELD_NAMES[squareID];
 		
 		System.out.println(latestMessage1 + " " + latestMessage2);
@@ -115,7 +115,7 @@ public class MatchingInfo_BaseImpl implements IMatchingInfo {
 	@Override
 	public void setMatchingFinderInfo(String netName, double probability) {
 		latestMessage1 = "Phase [" + currentPhase + "/" + phasesCount + "] " + (int) (100 * currentPhaseProgress) + "%";
-		latestMessage2 = "" + ProviderSwitch.getMLFrameworkDisplayName() + " " + (netName.substring(0, netName.lastIndexOf('.')) + " match " + (int) (100 * probability) + "%");
+		latestMessage2 = "" + ProviderSwitch.getMLFrameworkDisplayName() + ": " + (netName.substring(0, netName.lastIndexOf('.')) + " match " + (int) (100 * probability) + "%");
 		
 		System.out.println(latestMessage1 + " " + latestMessage2);
 	}
