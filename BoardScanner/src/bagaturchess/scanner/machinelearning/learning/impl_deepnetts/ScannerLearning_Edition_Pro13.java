76f174113190949690654d479f287cffdfc3ce36
MetatransApps <kitopchiyski@gmail.com>
2021-09-28 22:13:22 +0300

    Enable parallel learning of nets

--- BoardScanner/src/bagaturchess/scanner/machinelearning/learning/impl_deepnetts/ScannerLearning_Edition_Pro13.java
+++ BoardScanner/src/bagaturchess/scanner/machinelearning/learning/impl_deepnetts/ScannerLearning_Edition_Pro13.java
@@ -76,23 +76,23 @@ public class ScannerLearning_Edition_Pro13 implements Runnable {
 								)
         			);
 			
-        	/*learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_chess24com_set_1_extended/",
+        	learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_chess24com_set_1_extended/",
 													"dnet_chess24com_set_1_extended.dnet",
 													0.01f
 								)
-					);*/
+					);
         	
-        	/*learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_chesscom_set_1_extended/",
+        	learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_chesscom_set_1_extended/",
 													"dnet_chesscom_set_1_extended.dnet",
 													0.001f
 								)
-					);*/
+					);
         	
-        	/*learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_lichessorg_set_1_extended/",
+        	learningTasks.add(new ScannerLearning_Edition_Pro13("./datasets_deepnetts/dataset_lichessorg_set_1_extended/",
 													"dnet_lichessorg_set_1_extended.dnet",
 													0.01f
 								)
-					);*/
+					);
         	
 			ExecutorService executor = Executors.newFixedThreadPool(learningTasks.size());
 			
